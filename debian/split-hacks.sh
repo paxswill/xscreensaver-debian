26957673829338c39df6c6c49521a869409736f9
Tormod Volden <debian.tormod@gmail.com>
2009-08-11 00:04:43 +0200

    Define the package split in one database file
    The file debian/split-hacks.config has the format:
    <hack> <binary package> <optional comment>
    
    Empty lines or lines starting with # are ignored.
    
    The script debian/split-hacks.sh (called from debian/rules)
    processes the config file and moves files accordingly.
    
    Thanks to Andrew Starr-Bochicchio for initial work.

---
+++ debian/split-hacks.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+while read HACK PACKAGE COMMENT
+do
+    [ -z "$HACK" ] && continue
+    [ $HACK != "${HACK#\#}" ] && continue
+    dh_install -p$PACKAGE debian/tmp/usr/lib/xscreensaver/$HACK
+    dh_installman -p$PACKAGE debian/tmp/usr/share/man/man6/$HACK.6x
+    dh_install -p$PACKAGE debian/tmp/usr/share/xscreensaver/config/$HACK.xml
+    dh_install -p$PACKAGE debian/tmp/usr/share/applications/screensavers/$HACK.desktop
+done < debian/split-hacks.config
