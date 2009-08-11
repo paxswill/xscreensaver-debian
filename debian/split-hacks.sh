564c8574bbe89f81d1052f7381407a27333f5fc4
Tormod Volden <debian.tormod@gmail.com>
2009-08-12 01:46:17 +0200

    let split-hacks.sh bail out if missing hack, otherwise be happy
    We will allow missing man/xml/desktop files for now, but the hack
    itself must be found.

--- debian/split-hacks.sh
+++ debian/split-hacks.sh
@@ -4,8 +4,9 @@ while read HACK PACKAGE COMMENT
 do
     [ -z "$HACK" ] && continue
     [ $HACK != "${HACK#\#}" ] && continue
-    dh_install -p$PACKAGE debian/tmp/usr/lib/xscreensaver/$HACK
+    dh_install -p$PACKAGE debian/tmp/usr/lib/xscreensaver/$HACK || exit 1
     dh_installman -p$PACKAGE debian/tmp/usr/share/man/man6/$HACK.6x
     dh_install -p$PACKAGE debian/tmp/usr/share/xscreensaver/config/$HACK.xml
     dh_install -p$PACKAGE debian/tmp/usr/share/applications/screensavers/$HACK.desktop
 done < debian/split-hacks.config
+exit 0
