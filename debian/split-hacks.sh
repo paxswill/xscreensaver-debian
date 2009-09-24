a2d8d5f4e4272bb8a4f35056bfcdc66e748acbc0
Jose Luis Rivas <ghostbar38@gmail.com>
2009-09-23 21:49:09 -0430

    Added conditionals and created desktop file
    Added better conditionals for split-hacks.sh and created popsquares.desktop

--- debian/split-hacks.sh
+++ debian/split-hacks.sh
@@ -2,11 +2,15 @@
 
 while read HACK PACKAGE COMMENT
 do
-    [ -z "$HACK" ] && continue
-    [ $HACK != "${HACK#\#}" ] && continue
-    dh_install -p$PACKAGE debian/tmp/usr/lib/xscreensaver/$HACK || exit 1
-    dh_installman -p$PACKAGE debian/tmp/usr/share/man/man6/$HACK.6x
-    dh_install -p$PACKAGE debian/tmp/usr/share/xscreensaver/config/$HACK.xml
-    dh_install -p$PACKAGE debian/tmp/usr/share/applications/screensavers/$HACK.desktop
+	[ -z "$HACK" ] && continue
+	[ $HACK != "${HACK#\#}" ] && continue
+	dh_install -p$PACKAGE debian/tmp/usr/lib/xscreensaver/$HACK || exit 1
+	if [ -e debian/tmp/usr/share/man/man6/$HACK.6x ]; then
+		dh_installman -p$PACKAGE debian/tmp/usr/share/man/man6/$HACK.6x
+	fi
+	if [ -e debian/tmp/usr/share/xscreensaver/config/$HACK.xml ]; then
+	dh_install -p$PACKAGE debian/tmp/usr/share/xscreensaver/config/$HACK.xml
+	fi
+	dh_install -p$PACKAGE debian/tmp/usr/share/applications/screensavers/$HACK.desktop
 done < debian/split-hacks.config
 exit 0
